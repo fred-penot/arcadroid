e04d54a13836a811683c63ab94a1aed6603844a4
fred-penot <frederic.penot@gmail.com>
2017-06-10 19:50:01 +0200

    init4

--- app/doc/install.sh
+++ app/doc/install.sh
@@ -2,3 +2,6 @@ cd /var/www/html
 git clone https://github.com/fred-penot/arcadroid.git
 cd /var/www/html/arcadroid
 /usr/local/zend/bin/php composer install
+rm -f /var/www/html/arcadroid/vendor/knplabs/console-service-provider/Knp/Provider/ConsoleServiceProvider.php
+cp /var/www/html/arcadroid/app/doc/ConsoleServiceProvider.php /var/www/html/arcadroid/vendor/knplabs/console-service-provider/Knp/Provider/
+chmod -Rf 777 /var/www/html/arcadroid
